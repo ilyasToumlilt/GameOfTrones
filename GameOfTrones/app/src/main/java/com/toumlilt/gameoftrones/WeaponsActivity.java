411abeab641469dfa5331bbf6c445e7bf0cfa735
m2sar <m2sar@ppti-14-508-M-07.ufr-info-p6.jussieu.fr>
2016-01-29 21:40:00 +0100

    Affichage des armes avec images (Pour le moment par défaut)

--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/WeaponsActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/WeaponsActivity.java
@@ -1,13 +1,58 @@
 package com.toumlilt.gameoftrones;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class WeaponsActivity extends AppCompatActivity {
+public class WeaponsActivity extends Activity {
+
+    private Weapon ws[];
+
+    public WeaponsActivity(){
+        this(new Weapon[]{});
+    }
+
+    public WeaponsActivity(Weapon[] ws){
+        this.ws = ws;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weapons);
+
+        ArrayAdapter<Weapon> aad = new ArrayAdapter<Weapon>(this, android.R.layout.activity_list_item, this.ws){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                Context c = getContext();
+                LayoutInflater inf = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View rowView = convertView;
+
+                if(convertView == null ) {
+                    rowView = inf.inflate(android.R.layout.activity_list_item, parent, false);
+                    TextView textView = (TextView) rowView.findViewById(android.R.id.text1);
+                    ImageView imageView = (ImageView) rowView.findViewById(android.R.id.icon);
+                    textView.setText(
+                            "Arme : " + ws[position].getName() + " " +
+                            "PV : "+ws[position].getPv()  + " " +
+                            "Scope : " + ws[position].getScope()
+                    );
+                    imageView.setImageResource(android.R.drawable.btn_star);
+                }
+
+                return rowView;
+            }
+
+        };
+        ListView list=(ListView)findViewById(R.id.listview);
+        list.setAdapter(aad);
     }
+
 }
