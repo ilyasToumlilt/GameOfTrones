081fd9b46b496d97ccb0c13cbe462cd9d57c9325
m2sar <m2sar@ppti-14-409-04.ufr-info-p6.jussieu.fr>
2016-01-29 18:25:08 +0100

    Weapon model class

---
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/Weapon.java
@@ -0,0 +1,38 @@
+package com.toumlilt.gameoftrones;
+
+public class Weapon {
+    private String name;
+    private Integer pv;
+    private Integer scope;
+
+
+    public Weapon(String name, Integer pv, Integer scope) {
+        this.name = name;
+        this.pv = pv;
+        this.scope = scope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPv() {
+        return pv;
+    }
+
+    public void setPv(Integer pv) {
+        this.pv = pv;
+    }
+
+    public Integer getScope() {
+        return scope;
+    }
+
+    public void setScope(Integer scope) {
+        this.scope = scope;
+    }
+}
