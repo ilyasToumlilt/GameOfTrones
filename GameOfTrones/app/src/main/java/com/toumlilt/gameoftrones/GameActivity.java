c4eecfd1679ff0785edca68bf7e3bd128c783ca6
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2016-01-30 22:00:26 +0100

    Rotation and graphical changes

--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/GameActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/GameActivity.java
@@ -1,20 +1,16 @@
 package com.toumlilt.gameoftrones;
 
 import android.Manifest;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
@@ -68,11 +64,13 @@ public class GameActivity extends AppCompatActivity
     public final static String EXTRA_USERNAME = "com.toumlilt.gameottrones.USERNAME";
     public final static String EXTRA_USERDESC = "com.toumlilt.gameottrones.USERDESC";
 
+    /* Map attributes */
     private MapFragment mMapFragment;
     private GoogleMap googleMap;
     private Location mCurrentLocation;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private CircleOptions mCircleOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -316,6 +314,11 @@ public class GameActivity extends AppCompatActivity
                 this.player.setUsername(data.getStringExtra(EXTRA_USERNAME));
                 this.player.setUserdesc(data.getStringExtra(EXTRA_USERDESC));
                 this.updateNavigationViewData();
+
+                //this.mCircleOptions.center(new LatLng(mCurrentLocation.getLatitude() + 5, mCurrentLocation.getLongitude()));
+
+                //googleMap.addCircle(this.mCircleOptions);
+                //System.out.println("HEYHEY<<----");
             }
         }
     }
@@ -353,8 +356,8 @@ public class GameActivity extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap.setMyLocationEnabled(true);
-        this.googleMap.addMarker(new MarkerOptions().
-                position(googleMap.getCameraPosition().target).title("TutorialsPoint"));
+        //this.googleMap.addMarker(new MarkerOptions().
+        //        position(googleMap.getCameraPosition().target).title("TutorialsPoint"));
 
     }
 
@@ -387,9 +390,9 @@ public class GameActivity extends AppCompatActivity
     public void onConnected(Bundle bundle) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
-            System.out.println("--> OK");
+            System.out.println("--> PERMISSION OK");
         } else {
-            System.out.println("--> KO");
+            System.out.println("--> PERMISSION KO");
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     1);
@@ -424,11 +427,12 @@ public class GameActivity extends AppCompatActivity
                     .zoom(15).build();
 
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            googleMap.addCircle(new CircleOptions()
+            this.mCircleOptions = new CircleOptions()
                     .center(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
                     .radius(100)
                     .strokeColor(Color.RED)
-                    .fillColor(Color.BLUE));
+                    .fillColor(Color.argb(150, 168, 210, 224));
+            googleMap.addCircle(this.mCircleOptions);
         }
     }
 
@@ -442,14 +446,17 @@ public class GameActivity extends AppCompatActivity
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     this.locationSetup();
+
                 } else {
                     // TODO notif
                 }
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
+    }
+
+    private void updateCircleOptions() {
+
     }
 }
--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -25,6 +26,8 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_up);
 
         this.signUpButton = (Button) findViewById(R.id.signUpButton);
