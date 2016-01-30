8ee2a8af7dcfa112480f72e716cc3e7e7d06417c
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2016-01-30 19:14:37 +0100

    Added on simulator working version

--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.toumlilt.gameoftrones;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,11 @@ public class SignUpActivity extends AppCompatActivity {
 
     private Button   signUpButton;
     private EditText signUpEditText;
+
+    private SharedPreferences sharedPreferences;
+    public static final String USER_PREFS = "USERPREFS";
+    public static final String PREF_USERNAME = "USERNAMEkey";
+    public static final String PREF_USERDESC = "USERDESCkey";
 
     public final static String EXTRA_MESSAGE = "com.toumlilt.gameottrones.MESSAGE";
 
@@ -26,10 +33,26 @@ public class SignUpActivity extends AppCompatActivity {
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.background_sound);
         mediaPlayer.start();
 
+        this.sharedPreferences = getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
+
+        /* testing if a user already exists */
+        if(!sharedPreferences.getString(PREF_USERNAME, "").isEmpty()){
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, sharedPreferences.getString(PREF_USERNAME, ""));
+            startActivity(intent);
+        }
+
     }
 
     public void onClickSignUpButton(View view) {
         // Do something in response to signUpButton
+
+        /* saving the new username */
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PREF_USERNAME, signUpEditText.getText().toString());
+        editor.commit();
+
+        /* starting game activity */
         Intent intent = new Intent(this, GameActivity.class);
         intent.putExtra(EXTRA_MESSAGE, signUpEditText.getText().toString());
         startActivity(intent);
