e72c6f69b1f86910b5f0ee6506a11cf090414d35
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2016-01-31 01:44:33 +0100

    Added some cool documentation

--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/ProfileActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/ProfileActivity.java
@@ -8,15 +8,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+/**
+ * Activité gérant les modifications du profil du joueur
+ * à savoir son pseudo et son statut.
+ * Les modifications sont directements enregistées dans la
+ * sharedPreference prévue à cet effet
+ */
 public class ProfileActivity extends AppCompatActivity {
 
+    /**
+     * Pour la persistance des données du joueur
+     */
     private SharedPreferences sharedPreferences;
+    /**
+     * Identifiant de la shared pref
+     */
     public static final String USER_PREFS = "USERPREFS";
+    /**
+     * Clé pour le pseudo ( sharedPreferences )
+     */
     public static final String PREF_USERNAME = "USERNAMEkey";
+    /**
+     * Clé pour le statut ( sharedPreference )
+     */
     public static final String PREF_USERDESC = "USERDESCkey";
 
     @Override
@@ -24,6 +39,9 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        /* Ici je rècupère les données enregistrées dans la shared preference
+         * et je pré-rempli les deux EditText
+         */
         this.sharedPreferences = getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
         EditText usernameET = (EditText) findViewById(R.id.profileUsername);
         usernameET.setText(sharedPreferences.getString(PREF_USERNAME, ""));
@@ -31,6 +49,10 @@ public class ProfileActivity extends AppCompatActivity {
         userdescET.setText(sharedPreferences.getString(PREF_USERDESC, ""));
     }
 
+    /**
+     * This method is called when the view's save button is called
+     * @param view
+     */
     public void onClickProfileSaveButton(View view) {
         /* fetching Textviews */
         EditText usernameET = (EditText) findViewById(R.id.profileUsername);
--- GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
+++ GameOfTrones/app/src/main/java/com/toumlilt/gameoftrones/SignUpActivity.java
@@ -10,44 +10,77 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+/**
+ * Activité gérant l'enregistrement d'un joueur dans l'application.
+ * Cette Activité est également le point de l'application.
+ * Si un joueur est déjà enregistré, on passera directement à la GameActivity
+ */
 public class SignUpActivity extends AppCompatActivity {
 
-    private Button   signUpButton;
+    /**
+     * Champ de saisie du pseudo
+     */
     private EditText signUpEditText;
+    /**
+     * Bouton de validation
+     */
+    private Button   signUpButton;
 
+    /**
+     * Pour la musique
+     */
     private static MediaPlayer mediaPlayer;
 
+    /**
+     * Pour la persistance des données du joueur
+     */
     private SharedPreferences sharedPreferences;
+    /**
+     * Identifiant de la shared pref
+     */
     public static final String USER_PREFS = "USERPREFS";
+    /**
+     * Clé pour le pseudo ( sharedPreferences )
+     */
     public static final String PREF_USERNAME = "USERNAMEkey";
+    /**
+     * Clé pour le statut ( sharedPreference )
+     */
     public static final String PREF_USERDESC = "USERDESCkey";
-
+    /**
+     * Clé pour le message de l'intent
+     */
     public final static String EXTRA_MESSAGE = "com.toumlilt.gameottrones.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_up);
 
+        // getting UI Views
         this.signUpButton = (Button) findViewById(R.id.signUpButton);
         this.signUpEditText = (EditText) findViewById(R.id.signUpEditText);
 
+        // Setting up the music
         mediaPlayer= MediaPlayer.create(this, R.raw.background_sound);
         mediaPlayer.start();
 
+        // Setting up the sharedPreferences for data persistence
         this.sharedPreferences = getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
 
         /* testing if a user already exists */
         if(!sharedPreferences.getString(PREF_USERNAME, "").isEmpty()){
+            // if so we go directly to the GameActivity
             this.startGameActivity();
         }
 
     }
 
+    /**
+     * Gestion de l'évent click sur le bouton de validation
+     * @param view
+     */
     public void onClickSignUpButton(View view) {
-        // Do something in response to signUpButton
 
         /* saving the new username */
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -58,10 +91,15 @@ public class SignUpActivity extends AppCompatActivity {
         this.startGameActivity();
     }
 
+    /**
+     * Creation d'un Intent contenant les données du joueur
+     * Puis démarre la GameActivity
+     */
     private void startGameActivity() {
         Intent intent = new Intent(this, GameActivity.class);
         intent.putExtra(EXTRA_MESSAGE, sharedPreferences.getString(PREF_USERNAME, ""));
         intent.putExtra(GameActivity.EXTRA_USERDESC, sharedPreferences.getString(PREF_USERDESC, ""));
+
         startActivity(intent);
     }
 }
